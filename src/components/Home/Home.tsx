2d169aa6fee5cd05b3630c8be96d0661d3c79c6d
Rafał Matraś <43376031+Rafal-Matras@users.noreply.github.com>
2022-05-13 22:22:31 +0200

    creating Home component

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,9 +1,23 @@
 import React from "react";
+import {Link} from "react-router-dom";
+
+import car from '../../images/bugatti.png';
+
+import style from './Home.module.css';
 
 export const Home = () => {
 
     return (
-        <h1>Home</h1>
+        <div className={style.container}>
+            <div className={style.boxContainer}>
+                <img className={style.img} src={car} alt="samochód"/>
+                <div className={style.textBox}>
+                    <h1 className={style.h1}>Aplikacja</h1>
+                    <h2 className={style.h2}>Komis Samochodowy</h2>
+                </div>
+            </div>
+            <Link className={style.link} to='/login'>Zaloguj się</Link>
+        </div>
     );
 };
 
