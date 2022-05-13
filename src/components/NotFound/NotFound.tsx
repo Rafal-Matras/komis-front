74d652ea9aed977baaca219de82a474638623639
Rafał Matraś <43376031+Rafal-Matras@users.noreply.github.com>
2022-05-13 22:23:01 +0200

    creating NotFound component

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import {Link} from "react-router-dom";
+
+import style from './NotFound.module.css'
 
 export const NotFound = () => {
     return (
-        <h1>NotFound</h1>
+        <div className={style.container}>
+            <h1 className={style.h1}>404</h1>
+            <h2 className={style.h2}>Podany adres nie istnieje</h2>
+            <Link className={style.link} to='/login'>powrót do strony głównej</Link>
+        </div>
     );
 };
